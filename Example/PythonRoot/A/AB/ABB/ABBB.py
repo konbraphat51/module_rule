d7b9a253672075d0bb316756aa00c2ccde97fd67
konbraphat51 <konbraphat@gmail.com>
2023-11-24 21:39:08 +0900

    Add 6. example

--- Example/PythonRoot/A/AB/ABB/ABBB.py
+++ Example/PythonRoot/A/AB/ABB/ABBB.py
@@ -1,5 +1,4 @@
-#4. If need to depend on another package, create/update the Modules Dependency UML of the lowest-common package.
-#7. If using the same level-1 directory module: import by module name
+#6. If import the **co-directory module**: import by **module** name 
 from PythonRoot.A.AB.ABB.ABBA import abba
 
 def abbb():
